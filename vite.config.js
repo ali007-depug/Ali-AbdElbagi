c6b73d1c16d0abd7cd796ab38297ac823032b03f
ali007-depug <aliroma849@gmail.com>
2025-09-08 17:23:58 +0200

    Cut Down JavaScript in vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/Ali-AbdElbagi/",
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // split vendors
+        },
+      },
+    },
+  },
+});
