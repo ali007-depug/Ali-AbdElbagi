559d6d4e25b8ceaacf1d15ecf6e7f9fce285a1e0
ali007-depug <aliroma849@gmail.com>
2025-09-14 20:17:53 +0200

    add sitemap

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-import { VitePluginSitemap } from 'vite-plugin-sitemap'; 
+import sitemap from "vite-plugin-sitemap"; // Import the default export
 
 const routes = [
-  { path: '/', lastmod: new Date().toISOString() },          // Homepage
-  { path: '/works', lastmod: new Date().toISOString() },  // My Works page
-  { path: '/about', lastmod: new Date().toISOString() },  // About Me page
-  { path: '/skills', lastmod: new Date().toISOString() }, // My Skills page
+  { path: "/", lastmod: new Date().toISOString() }, // Homepage
+  { path: "/works", lastmod: new Date().toISOString() }, // My Works page
+  { path: "/about", lastmod: new Date().toISOString() }, // About Me page
+  { path: "/skills", lastmod: new Date().toISOString() }, // My Skills page
   // Add any other routes you have here
 ];
 
 export default defineConfig({
   base: "/Ali-AbdElbagi/",
-  plugins: [react(), tailwindcss(), VitePluginSitemap({
-    hostname: 'https://ali007.github.io/Ali-AbdElbagi/', 
-    routes: routes, 
-  }),],
-
-  
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      // Use it here as a function
+      hostname: "https://ali007.github.io/Ali-AbdElbagi/",
+      routes: routes,
+    }),
+  ],
 });
