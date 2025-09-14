e9fa30c0de5d87c39d990d8b65831e48f17c7612
ali007-depug <aliroma849@gmail.com>
2025-09-14 20:14:49 +0200

    add sitemap

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-import { visualizer } from "rollup-plugin-visualizer";
+import { VitePluginSitemap } from 'vite-plugin-sitemap'; 
+
+const routes = [
+  { path: '/', lastmod: new Date().toISOString() },          // Homepage
+  { path: '/works', lastmod: new Date().toISOString() },  // My Works page
+  { path: '/about', lastmod: new Date().toISOString() },  // About Me page
+  { path: '/skills', lastmod: new Date().toISOString() }, // My Skills page
+  // Add any other routes you have here
+];
 
 export default defineConfig({
   base: "/Ali-AbdElbagi/",
-  plugins: [react(), tailwindcss(),visualizer({ open: true })],
+  plugins: [react(), tailwindcss(), VitePluginSitemap({
+    hostname: 'https://ali007.github.io/Ali-AbdElbagi/', 
+    routes: routes, 
+  }),],
 
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"], // split vendors
-        },
-      },
-    },
-  },
+  
 });
