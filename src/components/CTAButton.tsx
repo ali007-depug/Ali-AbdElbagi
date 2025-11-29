a9d060e9c5f61c4b0d3a2dd55294a284468e86f2
ali007-depug <aliroma849@gmail.com>
2025-11-29 20:00:12 +0200

    fix CTA button width

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -22,7 +22,7 @@ export default function CTAButton({
         href={href}
         target="_blank"
         rel="noopener noreferrer"
-        className={`flex items-center px-8 py-4 w-fit ${customStyle}  transition-all duration-300 ease-in-out `}
+        className={`flex items-center px-8 py-4 min-w-[215px] justify-center ${customStyle}  transition-all duration-300 ease-in-out `}
       >
         <span className="block">{action} </span>
         <span className="block">{icon}</span>
