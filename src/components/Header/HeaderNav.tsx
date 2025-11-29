d6dd05527cf4a1bab25834ad26df9d0472df1364
ali007-depug <aliroma849@gmail.com>
2025-11-29 20:17:08 +0200

    fix header menu style

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -1,6 +1,8 @@
 import { useLocation, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import ChangeLangButton from "./LangButton";
+import { IoClose, IoMenu } from "react-icons/io5";
+
 
 interface NavProp {
   isMenuOpen: boolean;
@@ -41,7 +43,7 @@ export default function Nav({ isMenuOpen, toggleMenu }: NavProp) {
           to={to}
           onClick={isMenuOpen ? toggleMenu : undefined}
           className={`transition-all duration-100 ease-in-out ${
-            isActive ? "border-b-s-color border-b-[3px]" : ""
+            isActive ? "border-b-p-color border-b-[3px]" : ""
           }`}
         >
           {t(`header.menu.${link.link}`)}
@@ -55,32 +57,27 @@ export default function Nav({ isMenuOpen, toggleMenu }: NavProp) {
       {/* show hamburger menu on the mobile screen */}
       <button
         onClick={toggleMenu}
-        className="sm:hidden p-4 rounded-md focus:outline-none focus:ring-2 focus:ring-p-color cursor-pointer"
+        className="sm:hidden p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-p-color cursor-pointer"
         aria-expanded={isMenuOpen}
         aria-controls="mobileMenu"
         aria-label={
           isMenuOpen ? t("header.aria.closeMenu") : t("header.aria.openMenu")
         }
       >
-        {/* menu icon */}
-        <img
-          src={isMenuOpen ? "./icon-close.svg" : "./icon-hamburger.svg"}
-          alt=""
-          aria-hidden="true"
-        />
+        {isMenuOpen ? <IoClose size={25}/> : <IoMenu size={25}/>}
       </button>
 
       <ul
         id={isMenuOpen ? "mobileMenu" : "desktopMenu"}
         className={
           isMenuOpen
-            ? "flex flex-col justify-center items-center absolute right-0 mt-4 w-full text-center bg-n-color min-h-90 [&_li]:w-fit [&_a]:px-4 [&_a]:py-2 [&_a]:min-w-[90px] [&_a]:inline-block [&_a]:bg-bg-color [&_a]:text-p-color [&_a]:my-4 [&_a]:rounded-[5px] [&_a]:capitalize [&_a]:font-bold [&_a]:focus:outline-none [&_a]:focus:ring-2 [&_a]:focus:ring-offset-2 [&_a]:focus:ring-white pb-3"
+            ? "flex flex-col justify-center items-center absolute right-0 mt-4 w-full text-center bg-s-color min-h-90 [&_li]:w-fit [&_a]:px-4 [&_a]:py-2 [&_a]:min-w-[90px] [&_a]:inline-block [&_a]:bg-bg-color [&_a]:text-p-color [&_a]:my-4 [&_a]:rounded-[5px] [&_a]:capitalize [&_a]:font-bold [&_a]:focus:outline-none [&_a]:focus:ring-2 [&_a]:focus:ring-offset-2 [&_a]:focus:ring-white pb-3"
             : "hidden sm:flex sm:flex-wrap sm:justify-center space-x-2  [&_li]:w-fit  [&_a]:px-4 [&_a]:py-2  [&_a]:inline-block  [&_a]:capitalize [&_a]:font-bold [&_a]:text-p-color [&_a]:text-base  [&_a]:hover:border-b-[3px] [&_a]:hover:border-s-color"
         }
       >
         {links}
         {/* change lang buttons wrapper */}
-        <div className={`flex gap-2  ${isMenuOpen ? "" : ""} `}>
+        <div className={`flex gap-2 `}>
           <ChangeLangButton language="en" />
           <ChangeLangButton language="ar" />
         </div>
