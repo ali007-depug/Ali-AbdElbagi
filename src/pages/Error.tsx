47a6a6faf0fec0200cf9f08d3870f454a78ba11e
ali007-depug <aliroma849@gmail.com>
2025-11-29 21:03:17 +0200

    fix header menu style

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+import { IoHomeSharp } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import { useTranslation } from "react-i18next";
 interface ErrorProp {
-    msg:string;
+  msg: string;
 }
-export default function Error({msg}:ErrorProp) {
+export default function Error({ msg }: ErrorProp) {
+  const {t} = useTranslation();
   return (
-    <div className="flex items-center justify-center w-full min-h-[88dvh] bg-s-color">
-      <p className="font-bold text-2xl sm:text-3xl md:text-4xl text-white max-w-[40ch] text-center max-sm:px-5">✖️ {msg}</p>
+    <div className="flex flex-col gap-3 items-center justify-center w-full min-h-screen bg-p-color ">
+      <IoClose size={40} color="red" />
+      <p className="font-bold text-xl sm:text-2xl md:text-3xl text-white max-w-[40ch] text-center max-sm:px-5">
+         {msg}
+      </p>
+      <Link to="/" className="flex p-3 text-sm sm:text-xl  items-center gap-2 rounded-full bg-s-color hover:bg-s-color/80 transition-all duration-300 text-white font-bold ">
+      {t('errorPage.backToHome')}
+        <IoHomeSharp size={25} className="" color="white" />
+      </Link>
     </div>
   );
 }
